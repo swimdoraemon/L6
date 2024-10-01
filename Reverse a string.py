5db4109bfcb34b42a7282c597bd03052bbbc6b6b
swimdoraemon <indoaradhya@gmail.com>
2024-10-01 19:41:55 +0900

    Aradhya

---
+++ Reverse a string.py
@@ -0,0 +1,13 @@
+string = input("plese enter your own string : ")
+
+string2 = ("")
+
+for i in string:
+    string2 = i + string2
+
+
+print("\nThe original string = ", string)
+print("The Reversed String = ",string2)
+
+
+                                                                                                                                                                                                                                                                                                                                                                                      
